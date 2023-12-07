6ce827e3bf539447d1bfb2b28ac016f2b267d117
Hai Chi <chenxidev1129@gmail.com>
2023-12-07 20:05:30 +0800

    Add project files.

---
+++ taskmaster-api/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace taskmaster_api.Data.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ taskmaster-api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace taskmaster_api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
