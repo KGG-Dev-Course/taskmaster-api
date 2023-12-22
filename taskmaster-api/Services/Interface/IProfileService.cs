d6cc4a41a7f91b986d832e820c147bf1adaf76c5
Jones Ko <kgg0305@outlook.com>
2023-12-22 16:13:07 +0800

    Create IProfileService.cs

---
+++ taskmaster-api/Services/Interface/IProfileService.cs
@@ -0,0 +1,14 @@
+using taskmaster_api.Data.DTOs;
+using taskmaster_api.Data.DTOs.Interface;
+
+namespace taskmaster_api.Services.Interface
+{
+    public interface IProfileService
+    {
+        ICoreActionResult<ProfileDto> GetProfileById(int id);
+        ICoreActionResult<List<ProfileDto>> GetAllProfiles();
+        ICoreActionResult<ProfileDto> CreateProfile(ProfileDto profileDto);
+        ICoreActionResult<ProfileDto> UpdateProfile(int id, ProfileDto profileDto);
+        ICoreActionResult DeleteProfile(int id);
+    }
+}
